ab9738ab2961efaa1fc25fe375cc9a251aaede13
Nasif Zeehan <85276921+Nasif28@users.noreply.github.com>
2024-05-25 11:59:13 +0600

    final commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
+    sourcemap: true,
     chunkSizeWarningLimit: 1000, // Adjust this value to the desired limit in KB
+  },
+  server: {
+    open: true, // Automatically open the app in the browser
   },
   plugins: [react()],
 })
